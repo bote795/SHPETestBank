e0c8f6e0466fc3206b5a199ac8a69110aaf34b98
bote795 <nick_n_b@yahoo.com>
2015-05-18 00:11:50 -0400

    fixing a setting for devise in heroku

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,8 @@ module SHPETestBank
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
-
+    #divise to not acces the db or load models when precompiling your assets
+    config.assets.initialize_on_precompile = false
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
