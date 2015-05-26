4883b926637914642e1b9f834a2b9900fec31b52
bote795 <bote795@neo.tamu.edu>
2015-05-26 15:08:15 -0400

    removed a path that was causing it to say already signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
     end
   end
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || totals_test_entries_path
+    request.env['omniauth.origin'] ||  totals_test_entries_path
   end
 end
