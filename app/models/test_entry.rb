f55ee9059bf4779fc386e24e204965d74f7cdbad
bote795 <bote795@neo.tamu.edu>
2015-05-26 22:49:09 -0400

    allowed to be added from seeds for grades

--- app/models/test_entry.rb
+++ app/models/test_entry.rb
@@ -1,7 +1,7 @@
 class TestEntry < ActiveRecord::Base
   scope :at_test, ->(test_id){ where("\"classTestName_id\" = ?", test_id)}
   scope :at_class, ->(class_id){ where("\"className_id\" = ?", class_id)}
-  attr_accessible :link, :semester, :teacher, :className_id , :classTestName_id
+  attr_accessible :link, :semester, :teacher, :className_id , :classTestName_id, :grade
   belongs_to :ClassName
   belongs_to :ClassTestName
   validates_presence_of :link, :message => 'blank invalid value'
